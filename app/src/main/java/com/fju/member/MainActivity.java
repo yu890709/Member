c16586cf50172a4e194e5ec059711e0a623cddbc
miia <miia@com.tw>
2020-01-07 13:55:14 +0800

    init

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    String s;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
