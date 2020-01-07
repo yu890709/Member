7d3d64b6bd6b7495d5519757542ec39e6a703c91
miia <miia@com.tw>
2020-01-07 14:04:13 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,13 +2,32 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    String s;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private SharedPreferences pref;
+    private Intent intent;
+    private String gender;
+    private String age;
+    private String nickname;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("Member",MODE_PRIVATE);
+        intent = new Intent(this,NicknameActivity.class);
+        nickname = pref.getString("nickname","");
+        age = pref.getString("age","");
+        gender = pref.getString("gender","");
+        if(nickname.equals("")||age.equals("")||gender.equals("")){
+            startActivityForResult(intent,10);
+        }else{
+
+        }
     }
 }
--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -2,13 +2,18 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NicknameActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
+
     }
 }
